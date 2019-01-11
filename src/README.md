b475187c503e735e33bee2780e01462879a00e4a
fooTios <footios76@gmail.com>
2019-01-11 11:47:18 +0200

    added-stateful-component
    The withClass function can also return a stateful component instead of a functional.

--- src/README.md
+++ src/README.md
@@ -1,5 +1,9 @@
+### hoc
+
+```
 Here we wrap only the App and the Person
 components. So if you ommit the {...props}
 in the withClass hoc, you don't get their props.
 The withClass is not actually a component but
 just a fuction that returns a functional component.
+```
--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -2,15 +2,28 @@ import React, { Component } from 'react'
 
 // A function that doesn't take the props, but a configuration
 // Note! Do not manipulate-change state or props of the wrapped component!
+// const withClass = (WrappedComponent, className) => {
+// 	// If we ommit the {...props} we don't get
+// 	// the props of the component that we wrap
+
+// 	return props => (
+// 		<div className={className}>
+// 			<WrappedComponent {...props} />
+// 		</div>
+// 	)
+// }
+
 // You can also return a stateful component
 const withClass = (WrappedComponent, className) => {
-	// If we ommit the {...props} we don't get
-	// the props of the component that we wrap
-
-	return props => (
-		<div className={className}>
-			<WrappedComponent {...props} />
-		</div>
-	)
+	return class extends Component {
+		render() {
+			return (
+				<div className={className}>
+					<WrappedComponent {...this.props} />
+				</div>
+			)
+		}
+	}
 }
+
 export default withClass
